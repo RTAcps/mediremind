8f88defc5e79495110d094de27eaf73d3e49ebe6
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-03-29 16:56:12 -0300

    Main structure and tests

---
+++ backend/mediremind/src/main/java/com/mediremind/MediremindApplication.java
@@ -0,0 +1,13 @@
+package com.mediremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediremindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediremindApplication.class, args);
+	}
+
+}
