e4a0e04510daba6ceb138c9d47dfbbdc0f527ea0
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-03-31 11:52:09 -0300

    docs: change jamine to jest unit tests

---
+++ frontend/mediremind/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    globalSetup: 'jest-preset-angular/global-setup',
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/dist/'
+    ],
+    collectCoverage: false,
+    coverageDirectory: '<rootDir>/coverage',
+    coverageReporters: ['html', 'text', 'text-summary'],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/testing/'
+    ],
+    reporters: [
+        "default",
+        ["./node_modules/jest-html-reporter", {
+            "pageTitle": "Test Report"
+        }]
+      ]
+};
